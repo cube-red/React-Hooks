58f536883b765dfbdf772d1a6b58400e1a67c2a2
adi <bondreaditya99@gmail.com>
2023-02-08 00:47:56 +0530

    Added Individual Delete Feature

--- src/Hooks/UseState/UseStatewithArray.jsx
+++ src/Hooks/UseState/UseStatewithArray.jsx
@@ -6,7 +6,7 @@ const UseStatewithArray = () => {
         
     ]
     const [inputValue,setInputValue] = React.useState('');
-    const [array,setArray] = React.useState([]);
+    const [array,setArray] = React.useState(biodate);
 
     
 
@@ -23,18 +23,31 @@ const UseStatewithArray = () => {
         e.preventDefault();
         setArray([]);
     }
+
+    const handleDelete = (name) =>{
+        const mynewArray = array.filter((currElem) => {
+            return currElem !== name
+        })
+
+        setArray(mynewArray)
+    }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" value={inputValue} onChange={(event)=>setInputValue(event.target.value)}></input>
                 <button type="submit">Enter</button>
             </form>
+            <div>
             <ul style={{display:"flex", flexDirection:"column"}}>
                 {array.map((item, index) => (
+                <div style={{display:"flex",flexDirection:"row",gap:"20px"}}>
                 <li key={index}>{item}</li>
+                <button style={{backgroundColor:"red"}} onClick={()=>{handleDelete(item)}}X> X </button>
+                </div>
                 ))}
-                <button> X </button>
+                
             </ul>
+            </div>
 
             <button onClick={clearArray} style={{color:"red"}}>Clear!</button>
         </div>
